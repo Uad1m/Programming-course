d243bdfd69e6061a0701d65bce55b8aa952fc01d
Uad1m <71762086+Uad1m@users.noreply.github.com>
2020-09-29 15:09:26 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -5,133 +5,175 @@
 </head>
 <body>
 
-<h3>Введите условия банка</h3>
+<h3>Введите необходимую сумму:</h3>
 <form method="post" >
-    Название банка: <input type="text" name="bank_name" /><br><br>
-    Сумма кредита: <input type="int" name="crdt_s" /><br><br>
-    Процентная ставка: <input type="int" name="pers" /><br><br>
-    Комиссия банка: <input type="int" name="commis" /><br><br>
-    Дополнительная плата: <input type="int" name="addfee" /><br><br>
-
+    <input type="int" name="sum" autofocus><br><br>
     <input type="submit" class="submit-button" value="Отправить">
+
     <br><br>
-</form>
-<div>
+
+
 <?php
-			if(isset($_POST['bank_name']) 
-					&& isset($_POST['pers']) 
-					&& isset($_POST['crdt_s']) 
-					&& isset($_POST['commis'])
-					&& isset($_POST['addfee'])
-			){
- 
-    $bank_name=$_POST['bank_name'];
-    $pers = $_POST['pers'];
-    $crdt_s = $_POST['crdt_s'];
-    $commis = $_POST['commis'];
-    $addfee = $_POST['addfee'];
 
-    //Вычесляем количество месяцев, сумму выплаты банку, итоговую сумму выплаты.
-    		$bal_crdt = $crdt_s + $addfee;
-    		$smonth = 0;
-    		$bank_fee = 0;
-    		$bank_fee_total = 0;
-    		$s_total = 0;
-    		$p = 5000;//разовая ежемесячная выплата
-			
-				while ($bal_crdt > 0) {
-
-				
-//echo "Сейчас идет $smonth месяц<br>";
-				$bal_crdt= $bal_crdt-$p; // вычитываем разовую выплату в месяц с кредитного баланса
-//echo "после выплаты 5000 наш баланс : $bal_crdt<br>";
-				$bal_crdt= $bal_crdt + $commis;//считаем кредитный баланс с добавлением ежемесячной комиссии
-//echo "Мы платим 500 за комиссю и наша сумма теперь: $bal_crdt<br>";
-				$bank_fee = ($bal_crdt / 100) * $pers; //комиссия банка в этом месяце
-//echo "комиссия банка в этом месяце: $bank_fee<br>";
-				$bal_crdt= $bal_crdt + $bank_fee; //считаем кредитный баланс + процент за месяц
-//echo "наш кредит составляет: $bal_crdt<br>";
-
-				$bank_fee_total =$bank_fee_total + $bank_fee+$commis; // считаем сумму выплат в банк по процентам и ежемесячной комиссии
-//echo "Сумма комиссии 500+%: $bank_fee_total<br><br>";
-				$smonth = $smonth + 1; // счетчик количества месяцев
-				
-
-				if ($bal_crdt<$p) {   // сравниваем остаток тела кредита с ежимесячным платежем, если меньше то добавляем общей сумме выплат
-						$s_total=round($bal_crdt);
-//echo "остаток суммы для выплаты $s_total<br>";
-						$bal_crdt=0;
-
-						$bank_fee_total=round($bank_fee_total);
-//echo "Общая сумма это $s_total + $bank_fee_total + ($smonth * $p)";
-						$s_total =$s_total + $bank_fee_total+ $smonth * $p ;//считаем общую сумму выплат по кредиту
-
-					}
-				}
-			
-//    echo "Банк: $bank_name <br> 
-//    Сумма кредита: $crdt_s <br> 
-//    Процентная ставка: $pers % <br>
-//    Комиссия банка: $commis <br>
-//    Количество месяцев для выплаты: $smonth <br>
-//    Итоговая сумма выплат по кредиту: $s_total <br>
-//    "
-//    ;
-	}
-//	$info = array(
-//    "$s_total"  => $bank_name,
-
-//);
-//  $min= min($info);
-//  echo "Самое выгодное предложение: $min <br><br>";
-//	foreach ($info as $k => $value) {
-//		echo "<br>$k $value";
-		
-//	}
+//берем массив наличных купюр в банкомате
+$string_data = file_get_contents("data.txt");
+$array = unserialize($string_data);
+$a=$array;
+$k= max(array_keys($a)); // количество банкнот в банкомате
+$n=$_POST['sum']; // сумма которую надо выдать
+error_reporting (E_ALL ^ E_NOTICE); // отключил отображение Notice: Undefined offset
+echo "$n грн : "; //выводим введенную сумму для снятия
+$p=0; //счетчик количества необходимых купюр к выдаче
 
 
-try {
-    $dataStr = file_get_contents('data.json');
-    $dataArray = json_decode($dataStr, true);
-} catch (Exception $ex) {
-    $dataArray = [];
+$left=array_sum($a);
+if ($n==0) {
+	echo "Неверная сумма (ноль невозможно выдать) <br>";
+}
+while ($n>0) {
+	
+
+if ($n>$left) {
+	echo "Недостаточно денег<br>";
+	break;
 }
 
-$dataArray[] = ['Bank name: '=> $bank_name,
-					 'Credit sum: ' => $crdt_s, 
-					 'Percent rate: ' => $pers , 
-					 'Commission of the bank: ' => $commis,
-					 'Additional fee: ' => $addfee,
-					 'Months to pay off the loan: ' => $smonth+1,
-					 'Payment to the bank total: '=> $bank_fee_total,
-					 'The total amount of payments on the loan: '=> $s_total];
-$newDataSrt = json_encode($dataArray, JSON_PRETTY_PRINT);
-$result = file_put_contents('data.json', $newDataSrt);
+if ($n %5 != 0) {
+	echo "Неверно указана сумма (не кратна 5, невозможно выдать)<br>";
+	break;
+}
+$temp=array();
+$temp=$a;
+$text=" ";
+
+	if ($n>0 & $n>=1000){
+		for ($i=0; $i<=$k; $i++) {
+		if (($a[$i])==1000 & $n>=1000 & null !== isset($a[$i])) {
+			$n=$n-1000;
+			unset($a[$i]);
+			$p++;
+			}
+		}
+			$text=" $p x 1000, ";
+		}
+
+		$k=max(array_keys($a));
+		$p=0;
+
+		if ($n>0 & $n>=500) {
+
+			for ($i=0; $i<=$k; $i++) { 		
+			if (($a[$i])==500 & $n>=500 & null !== isset($a[$i])) {
+			$n=$n-500;
+			unset($a[$i]);
+			$p++;
+			}
+		
+		}
+			$text=$text. " $p x 500, ";
+		}
+
+		$k=max(array_keys($a));
+		$p=0;
+
+		if ($n>0 & $n>=200) {
+			for ($i=0; $i<=$k; $i++) {
+			if (($a[$i])==200 & $n>=200 & null !== isset($a[$i])) {
+			$n=$n-200;
+			unset($a[$i]);
+			$p++;
+			}
+		}
+			$text=$text. " $p x 200, ";
+		}
+
+		$k=max(array_keys($a));
+		$p=0;
+
+		if ($n>0 & $n>=100) {
+			for ($i=0; $i<=$k; $i++) { 
+			if (($a[$i])==100 & $n>=100 & null !== isset($a[$i])) {
+			$n=$n-100;
+			unset($a[$i]);
+			$p++;
+				}
+			}
+			$text=$text. " $p x 100, ";
+		}
+		
+		$k=max(array_keys($a));
+		$p=0;
+
+		if ($n>0 & $n>=50) {
+			for ($i=0; $i<=$k; $i++) { 
+			if (($a[$i])==50 & $n>=50 & null !== isset($a[$i])) {
+			$n=$n-50;
+			unset($a[$i]);
+			$p++;
+			}
+		}
+			$text=$text. " $p x 50, ";
+		}
+
+		$k=max(array_keys($a));
+		$p=0;
+
+		if ($n>0 & $n>=20) {
+			for ($i=0; $i<=$k; $i++) { 
+			if ($a[$i]==20 & $n>=20 & null !== isset($a[$i])) {
+			$n=$n-20;
+			unset($a[$i]);
+			$p++;
+			}
+		}
+			$text=$text. " $p x 20, ";
+		}
+
+		$k=max(array_keys($a));
+		$p=0;
+
+		if ($n>0 & $n>=10) {
+			for ($i=0; $i<=$k; $i++) { 
+			if (($a[$i])==10 & $n>=10 & null !== isset($a[$i])) {
+			$n=$n-10;
+			unset($a[$i]);
+			$p++;
+			}
+		}
+				$text=$text. " $p x 10, ";
+		}
+
+		$k=max(array_keys($a));
+		$p=0;
+
+		if ($n>0 or $n>=5 ) {
+			for ($i=0; $i<=$k; $i++) { 
+			if (($a[$i])==5 & $n>=5 & isset($a[$i]) == true ) {
+			$n=$n-5;
+			unset($a[$i]);
+			$p++;
+			}
+		}
+			$text=$text. " $p x 5 ";
+		}
+		//недостаточно мелких купюр, возврат суммы к предыдущему значению
+		if($n>0){ 
+		echo "Невозможно выдать (недостаточно мелких купюр) <br> ";
+		$a=$temp;
+		break;
+		}
+	echo "$text <br>"; //вывод необходимых купюр для выдачи суммы
+}
+	//Вывод остатка суммы денег в банкомате
+	$left=array_sum($a); 
+	echo "(Остаток : $left) <br>";
+// помещаем в основной массив наличных купюр - новый массив из оставшихся купюр
+$string_data = serialize($a);
+file_put_contents("data.txt", $string_data);
 
 
-try {
-            $dataStr = file_get_contents('data.json');
-            $dataArray = json_decode($dataStr, true);
-        } catch (Exception $ex) {
-            $dataArray = [];
-        }
-
-        foreach ($dataArray as $dataRow) {
-            echo "Название банка: " . $dataRow['Bank name: '] . "<br>";
-//            echo "Сумма кредита: " . $dataRow['Credit sum: '] . "<br>";
-//            echo "Процентная ставка: " . $dataRow['Percent rate: '] . "<br>";
-//            echo "Комиссия банка: " . $dataRow['Commission of the bank: '] . "<br>";
-//            echo "Дополнительная плата: " . $dataRow['Additional fee: '] . "<br>";
-            echo "Количество месяцев для выплаты: " . $dataRow['Months to pay off the loan: '] . "<br>";
-//            echo "Выплата банку итог: " . $dataRow['Payment to the bank total: '] . "<br>";
-            echo "Итоговая сумма выплат по кредиту: " . $dataRow['The total amount of payments on the loan: '] . "<br><br>";
-
-
-        } 
 ?>
 
-
-</div>
 
 </body>
 </html>
